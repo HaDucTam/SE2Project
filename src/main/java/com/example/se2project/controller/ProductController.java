d3099d95af178d9de5d0f2a8dbad63758152ad96
HaDucTam <77770297+HaDucTam@users.noreply.github.com>
2022-04-22 17:22:48 +0700

    Update ProductController.java

--- src/main/java/com/example/se2project/controller/ProductController.java
+++ src/main/java/com/example/se2project/controller/ProductController.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 @Controller
 @SessionAttributes("userId")
+@RequestMapping({"/product"})
 public class ProductController {
     @Autowired
     ProductService productService;
@@ -34,64 +35,30 @@ public class ProductController {
     CategoryService categoryService;
     @Autowired
     UserService userService;
-    @GetMapping("/product/addProduct")
-    public String addProductForm(Model model) {
-        model.addAttribute("products",new ProductDto());
-        model.addAttribute("categories",categoryService.findAll());
-        return "adminPages/products/productAdd";
-    }
-    @GetMapping("/product/category/{id}")
-    public String productByCategory(@PathVariable Long id, Model model) {
-        model.addAttribute("allCategory", categoryService.findAll());
-//        model.addAttribute("cartCount")?
-        model.addAttribute("products", productService.getProductByCategoryId(id));
-        return "homePage";
-    }
-    @GetMapping("/product/addCategory")
-    public String addCategoryForm(Model model) {
-        model.addAttribute("category",new Category());
-        return "categoryAdd";
-    }
-    @PostMapping("/addCategory/save")
-    public String saveCategory(@RequestParam(value = "name", required = false, defaultValue = "empty name") String name,
-                               @RequestParam(value = "description", required = false, defaultValue = "empty description") String description) {
-        Category category = new Category();
-        category.setName(name);
-        category.setDescription(description);
-        categoryService.add(category);
-        return "redirect:/";
-    }
-    @PostMapping("/addProduct/save")
-    public String saveProduct(@ModelAttribute("products") ProductDto productDto,
-                              @RequestParam(value = "image", required = false) MultipartFile image) throws IOException {
 
-        String fileName = StringUtils.cleanPath(image.getOriginalFilename());
-        Product product = new Product();
-        product.setProductId(productDto.getId());
-        product.setName(productDto.getName());
-        product.setDetail(productDto.getDetail());
-        product.setPrice(productDto.getPrice());
-        product.setCategory(categoryService.findById(productDto.getCategoryId()).get());
-        product.setImage(fileName);
-        Product savedProduct = productService.add(product);
-        String uploadDir = "product-image/" + savedProduct.getProductId();
-        Path uploadPath = Paths.get(uploadDir);
-        if(!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
-        }
-        try {
-            InputStream inputStream = image.getInputStream();
-            Path filePath = uploadPath.resolve(fileName);
-            System.out.println(filePath.toFile().getAbsolutePath());
-            Files.copy(inputStream,filePath, StandardCopyOption.REPLACE_EXISTING);
-        }catch (IOException e) {
-            throw new IOException("could not save uploaded file: " + fileName);
-        }
-        return "redirect:/";
-    }
-    @GetMapping("/product/addToCart/{id}")
+
+
+    //    @GetMapping("/shop/category/{id}")
+//    public String productByCategory(@PathVariable Long id, Model model) {
+////        model.addAttribute("cartCount")?
+//
+////        model.addAttribute("allCategory", categoryService.findAll());
+//        model.addAttribute("products", productService.getProductByCategoryId(id));
+//        return "shopPage";
+//    }
+//    @GetMapping("/shop")
+//    public String viewShop(Model model) {
+//        List<Product> products = productService.findAll();
+////        if(!products.isEmpty()){
+//            model.addAttribute("products", products);
+//            model.addAttribute("allCategory", categoryService.findAll());
+////        }
+//        return "shopPage";
+//    }
+
+    @GetMapping("/addToCart/{id}")
     public String addToCart(@PathVariable("id") Long id,
-                             HttpServletRequest servletRequest, HttpSession session) {
+                            HttpServletRequest servletRequest, HttpSession session) {
 
 //        User u = (User) model.getAttribute("userId");
         Long ids = (Long) servletRequest.getSession().getAttribute("userId");
@@ -115,7 +82,7 @@ public class ProductController {
         }
         return "redirect:/cartProductList";
     }
-//    @GetMapping("/")
+    //    @GetMapping("/")
 //    public String viewProduct(Model model) {
 //        List<Product> products = productService.findAll();
 //        if(!products.isEmpty()){
@@ -141,5 +108,12 @@ public class ProductController {
         }
         return -1;
     }
+    @GetMapping(value = "/{id}")
+    public String getProductById(@PathVariable(value = "id") Long id, Model model) {
+        Product product = productService.findById(id).get();
+        model.addAttribute("productById", product);
+        return "productDetail";
+    }
+
 
 }
